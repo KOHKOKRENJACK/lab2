e7e487954200e8d3dac3f370ef9283b67d48a4a6
KOHKOKRENJACK <jackkoh.19@ichat.sp.edu.sg>
2025-11-04 14:56:41 +0800

    calc bmi to return(-1 , 0 , 1)

--- bmi.py
+++ bmi.py
@@ -8,9 +8,13 @@ def calculate_bmi(height, weight):
 
     if bmi<18.5:
         print("You are underweight.")
+        return -1
     elif 18.5 <= bmi <24.9:
         print("You are normal weight. ")
+        return 0
     elif bmi >24.9:
         print("You are overweight. ")
+        return 1
 
-calculate_bmi(weight=57, height=1.73)
+result = calculate_bmi(weight=57, height=1.73)
+print("Returned value : ", result)
